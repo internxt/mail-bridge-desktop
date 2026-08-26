395238bf7734b5bf0ef1e0eb570b29bda39fb27f
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-08-26 13:04:47 +0200

    fix: remove useless file and add it to git ignore

---
+++ internal/logger/logger.go
@@ -0,0 +1,16 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+type Logger struct{ l *log.Logger }
+
+func New(scope string) *Logger {
+	return &Logger{l: log.New(os.Stderr, "["+scope+"] ", log.LstdFlags|log.Lmsgprefix)}
+}
+
+func (lg *Logger) Info(format string, a ...any)  { lg.l.Printf("INFO  "+format, a...) }
+func (lg *Logger) Warn(format string, a ...any)  { lg.l.Printf("WARN  "+format, a...) }
+func (lg *Logger) Error(format string, a ...any) { lg.l.Printf("ERROR "+format, a...) }
