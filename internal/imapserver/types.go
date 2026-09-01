d4bbf33d356d9722e91aa629b51f32b4391f13ef
Alexis Mora <Alexis.mora.sanchez@gmail.com>
2026-09-01 13:08:20 +0200

    chore: add credentials to smtp server

--- internal/imapserver/types.go
+++ internal/imapserver/types.go
@@ -13,8 +13,8 @@ import (
 // UnlockedSession identifies an account whose authentication and encryption
 // keys have already been unlocked by the desktop application.
 //
-// TODO(auth): add the minimal key/session handle required by the production
-// connector. Do not add an account password to this type: passwords are used
+// TODO(auth): add the minimal key/session handle required by the production connector.
+// Do not add an account password to this type: passwords are used
 // only by the application while unlocking this session.
 type UnlockedSession struct {
 	AccountID string
