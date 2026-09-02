81f79bc9e4005f05fd72c9c5966ca5b6cd0595cc
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-09-02 10:56:05 +0200

    fix: remove useless files

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import config from '@internxt/css-config'
-
-export default {
-  ...config,
-  content: [
-    './node_modules/@internxt/ui/dist/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}'
-  ]
-}
